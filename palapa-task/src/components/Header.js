8e6363bdbcd84e73f6a269b9370c43c5f075ed90
pjotreker <piotrek_rychlicki@interia.pl>
2023-01-11 12:43:17 +0100

    corrected Header.js and App.css

--- palapa-task/src/components/Header.js
+++ palapa-task/src/components/Header.js
@@ -5,7 +5,9 @@ function Header() {
         <div
           className="App-sign-in-window-header"
         >
-            <img className="App-header-logo-picture" alt="palapa-logo"></img>
+            <div className="App-header-logo-picture-holder">
+              <img className="App-header-logo-picture" alt="palapa-logo" src="../logo.png" />
+            </div>
             <div className="App-header-headline">
                 <a className="App-header-headline-text">Sign in to your account</a>
             </div>
